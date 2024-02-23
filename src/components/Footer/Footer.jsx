d2d34a2edee02d3c0d69a3e66c3f74be93da4b98
Intrum1 <skoropadskiy1979@gmail.com>
2024-02-23 13:33:05 +0100

    1

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,10 +1,27 @@
 import { ContainerWrap } from '../App/App.styled';
+import sprite from 'assets/icons/sprite.svg';
+import { FooterTitle } from './Footer.styled';
 
 const Footer = () => {
   return (
     <footer>
       <ContainerWrap>
-        <adress>Prague, Czech Republic</adress>
+        <FooterTitle>Účetnictví a poradenství</FooterTitle>
+        <adress>
+          Prague, Czech Republic <br />
+        </adress>
+        <svg width="30">
+          <use href={`${sprite}#icon-instagram`}></use>
+        </svg>
+        <svg width="30">
+          <use href={`${sprite}#icon-address`}></use>
+        </svg>
+        <svg width="30">
+          <use href={`${sprite}#icon-facebook`}></use>
+        </svg>
+        <svg width="30">
+          <use href={`${sprite}#icon-phone`}></use>
+        </svg>
       </ContainerWrap>
     </footer>
   );
---
+++ src/components/Footer/Footer.styled.jsx
@@ -0,0 +1,7 @@
+import { styled } from 'styled-components';
+
+export const FooterTitle = styled.h2`
+  font-size: 40px;
+  font-weight: 400;
+  line-height: 1.2;
+`;
